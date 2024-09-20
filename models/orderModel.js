c4804f277f165f728a64c9a9c82a9c44a602ad35
Ashikur Rahman Bhuiyan <ashikurrb85@gmail.com>
2024-09-20 23:01:54 +0600

    update type

--- models/orderModel.js
+++ models/orderModel.js
@@ -11,7 +11,7 @@ const orderSchema = new mongoose.Schema({
         required: true,
     },
     accNumber: {
-        type: Number,
+        type: String,
         required: true,
     },
     trxId: {
